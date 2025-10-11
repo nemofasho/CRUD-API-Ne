9a34aafb1e5c2636659cd031f8f255efa0282b2f
nemofasho <nehemiahmcclain11@gmail.com>
2025-10-10 20:29:51 -0400

    initial commit

---
+++ src/main/java/com/nmcclain/CRUDAPIdemo/CrudapIdemoApplication.java
@@ -0,0 +1,13 @@
+package com.nmcclain.CRUDAPIdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudapIdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudapIdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmcclain/CRUDAPIdemo/CrudapIdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmcclain.CRUDAPIdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudapIdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
